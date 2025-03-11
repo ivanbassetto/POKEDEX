c0f77285deb6aca037d03ddb1cfdf7061e56b244
ivanbassetto <ivanbassetto@msn.com>
2025-03-11 11:32:33 -0300

    HEADER
    ORGANIZANDO ELEMENTOS

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,18 @@ const Header: React.FC = () => {
         <img src="/diversos/Pokeball.png" alt="" />
         <h1>Pokédex</h1>
       </div>
-      <div className="partB_header">      
-        <input type="text" />
-        <img src="" alt="" />
-      </div>
+      <div className="partB_header">
+  <div className="input-container">
+    <span className="material-symbols-rounded search-icon">search</span>
+    
+    <input type="text" placeholder="Search" />
+  </div>
+  <div className="circle">
+    #
+  </div>
+</div>
+
+
     </header>
   );
 };
